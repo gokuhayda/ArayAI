5acdd67edaf1c45e23478bd59ca701db17dbb264
gokuhayda <82773670+gokuhayda@users.noreply.github.com>
2025-04-15 11:13:25 -0300

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://980d-201-13-16-41.ngrok-free.app/chat';
+const API_URL = 'https://0839-201-13-16-41.ngrok-free.app/chat';
 
 document.addEventListener("DOMContentLoaded", function () {
     // Cria o botão flutuante para o chatbot
